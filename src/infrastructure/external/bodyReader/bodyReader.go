7346248f016d03ff32efc7f412b1266037b24f9f
AERM2018 <retana.martinez.angel.eduardo@gmail.com>
2022-08-03 18:29:07 -0500

    Done: Body reader added - Read more than once the req body in the db validator middleware

---
+++ src/infrastructure/external/bodyReader/bodyReader.go
@@ -0,0 +1,17 @@
+package bodyreader
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+
+	"github.com/kataras/iris/v12"
+)
+
+func ReadBodyAsJson(ctx iris.Context, dest any, consumeBody bool) {
+	body, _ := io.ReadAll(ctx.Request().Body)
+	json.Unmarshal(body, dest)
+	if !consumeBody {
+		ctx.Request().Body = io.NopCloser(bytes.NewBuffer(body))
+	}
+}
---
+++ src/infrastructure/http/middlewares/dbValidator.go
@@ -0,0 +1,66 @@
+package middlewares
+
+import (
+	"cecan_inventory/src/adapters/helpers"
+	"cecan_inventory/src/domain/models"
+	bodyreader "cecan_inventory/src/infrastructure/external/bodyReader"
+	datasources "cecan_inventory/src/infrastructure/external/dataSources"
+	"fmt"
+
+	"github.com/kataras/iris/v12"
+)
+
+type DbValidator struct {
+	MedicineDataSrc datasources.MedicinesDataSource
+}
+
+func (dbVal DbValidator) IsMedicineInCatalogByKey(ctx iris.Context) {
+	var (
+		httpRes  models.Responser
+		medicine models.Medicine
+	)
+	medicineKey := ctx.Params().GetString("key")
+	if medicineKey == "" { // Get medicine key from body when it's not found in the url
+		bodyreader.ReadBodyAsJson(ctx, &medicine, false)
+		medicineKey = medicine.Key
+	}
+	isMedicine := dbVal.MedicineDataSrc.DbPsql.Unscoped().First(&medicine, medicineKey).RowsAffected
+	if ctx.Request().Method == "POST" {
+		if isMedicine == 1 {
+			httpRes = models.Responser{
+				StatusCode: iris.StatusNotFound,
+				Message:    fmt.Sprintf("El medicamento con clave: %v ya existe.", medicineKey),
+			}
+			helpers.PrepareAndSendMessageResponse(ctx, httpRes)
+			return
+		}
+	} else {
+		if isMedicine == 0 {
+			httpRes = models.Responser{
+				StatusCode: iris.StatusNotFound,
+				Message:    fmt.Sprintf("El medicamento con clave: %v no existe.", medicineKey),
+			}
+			helpers.PrepareAndSendMessageResponse(ctx, httpRes)
+		}
+
+	}
+	ctx.Next()
+}
+
+func (dbVal DbValidator) IsMedicineWithName(ctx iris.Context) {
+	var (
+		httpRes  models.Responser
+		medicine models.Medicine
+	)
+	medicineKey := ctx.Params().GetString("key")
+	bodyreader.ReadBodyAsJson(ctx, &medicine, false)
+	isMedicine := dbVal.MedicineDataSrc.DbPsql.Where("key != ? AND name = ?", medicineKey, medicine.Name).Find(&models.Medicine{}).RowsAffected
+	if isMedicine != 0 {
+		httpRes = models.Responser{
+			StatusCode: iris.StatusNotFound,
+			Message:    fmt.Sprintf("Ya existe un medicamento con el nombre: %v.", medicine.Name),
+		}
+		helpers.PrepareAndSendMessageResponse(ctx, httpRes)
+	}
+	ctx.Next()
+}
